06d787bab6d4ab31ecdce453be61568d2d985166
sidiDev <sididev3@gmail.com>
2025-08-20 20:39:57 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Stage, Layer } from "react-konva";
+
+function App() {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer></Layer>
+    </Stage>
+  );
+}
+
+export default App;
